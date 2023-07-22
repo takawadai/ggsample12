43b0bcf64b6ed514f9c55c50a731446831d9048b
takas <s246017@wakayama-u.ac.jp>
2023-07-22 15:12:31 +0900

    鏡らしくなった．しかし，鏡の中からウサギの一部がはみ出てるのがおかしい．仕様の問題かな？

--- ggsample12.cpp
+++ ggsample12.cpp
@@ -107,7 +107,7 @@ int GgApp::main(int argc, const char* const* argv)
     const auto mp{ ggPerspective(0.5f, window.getAspect(), 1.0f, 15.0f) };
 
     // 鏡像用のシェーダの選択
-    mirror.use(mp, mv * mm * mt * mr * mtm * window.getRotationMatrix(), lightBuffer);
+    mirror.use(mp, mt * mr * mtm * (mv * mm  * window.getRotationMatrix()), lightBuffer);
     lightBuffer.loadPosition(reflected.data());
 
     // 鏡像の描画
