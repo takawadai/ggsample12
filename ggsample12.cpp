7513ccd934b02dada6e9a5e8e0c607a5c3ad8f60
takas <s246017@wakayama-u.ac.jp>
2023-07-22 15:37:10 +0900

    正像のワールド座標上の位置が分かれば解けるのだけれども，位置の求め方が分からん．

--- ggsample12.cpp
+++ ggsample12.cpp
@@ -107,6 +107,7 @@ int GgApp::main(int argc, const char* const* argv)
     const auto mp{ ggPerspective(0.5f, window.getAspect(), 1.0f, 15.0f) };
 
     // 鏡像用のシェーダの選択
+    //このままでは鏡から物体がはみ出る
     mirror.use(mp, mt * mr * mtm * (mv * mm  * window.getRotationMatrix()), lightBuffer);
     lightBuffer.loadPosition(reflected.data());
 
